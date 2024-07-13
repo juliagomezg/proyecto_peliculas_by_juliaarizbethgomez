9b119c6127418e4b1a58b01ef1bd5d50fb6299c4
Julia Gomez <juliagomez19@gmail.com>
2024-07-13 15:41:40 -0700

    conexión a la base de datos funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const apiReadAccessToken = import.meta.env.VITE_API_ACCESS_TOKEN;
+
+  // Fetch movies from API
+  useEffect(() => {
+
+    const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer  ${apiReadAccessToken}`
+      }
+    };
+
+    fetch(url, options)
+      .then(res => res.json())
+      .then(data => setMovies(data.results))
+      .then(data => console.log(data.results))
+      .catch(err => console.error('error:' + err));
+
+  }, [])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {
+
+        movies.map((movies) => (
+          <>
+            <div
+              key={movies.title}
+            >
+              <h1>{movies.title}</h1>
+              <img style={{ width: 200 }} src={`https://image.tmdb.org/t/p/original/${movies.poster_path}`} alt={movies.title} />
+            </div>
+          </>
+
+        ))
+      }
+    </div>
   )
 }
 
